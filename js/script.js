d00531585f6068df364a872e44599ad7f5d3b12c
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-03-25 18:03:47 +0000

    savepoint

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const radiusNumber = document.getElementById("radius-number").value
+
+  // process
+  const areaOfCircle = (2 * Math.PI * radiusNumber)
+  const roundedAnswer = areaOfCircle.toFixed(2)
+
+  // output
+  document.getElementById("area-of-a-cicle").innerHTML =
+    "The area of this circle is: " + roundedAnswer + " cm²."
+}
